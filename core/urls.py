2129714edd6cb661e4a0c5378d7e676b1962169d
JP <jppaiva1706@gmail.com>
2026-04-17 08:57:17 -0300

    Implement home view and update URL configurations; remove unused user-related files and CSS styles.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from users import views
+from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
     path('players/', include('players.urls'))
 ]
