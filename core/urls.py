3b2423c8f358172ede462ecefa2372c2b9a25382
JP <jppaiva1706@gmail.com>
2026-04-21 18:57:23 -0300

    Add media file serving to URL patterns in urls.py; remove static URL handling to streamline media access.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
@@ -11,6 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('players/', include('players.urls')),
     path('teams/', include('teams.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
