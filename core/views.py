3c73dc01595e4f1a6ecdbd5ceda7feec1be696be
brxno3107 <breno310702@gmail.com>
2026-04-21 13:32:07 -0300

    Join Requests adicionado no dashboard de dono de time

--- core/views.py
+++ core/views.py
@@ -10,9 +10,12 @@ def dashboard(request):
     player = request.user.player
     received_invites = player.invites_received.filter(status='pending').select_related('team')
     sent_invites = None
+    pending_requests = None
     if player.is_team_owner:
         sent_invites = player.owned_team.invites.filter(status='pending').select_related('player__user')
+        pending_requests = player.owned_team.join_requests.filter(status='pending').select_related('player__user')
     return render(request, 'app/dashboard.html', {
         'received_invites': received_invites,
         'sent_invites': sent_invites,
+        'pending_requests': pending_requests,
     })
