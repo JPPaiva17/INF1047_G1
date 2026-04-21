92ed2c91d522b8677670cea6ab5ddd793e73082e
brxno3107 <breno310702@gmail.com>
2026-04-21 13:41:34 -0300

    Funcionalidade de sair do time, kick de time e notificação criadas

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('notification/<int:pk>/read/', views.mark_notification_read, name='mark_notification_read'),
     path('admin/', admin.site.urls),
     path('players/', include('players.urls')),
     path('teams/', include('teams.urls')),
